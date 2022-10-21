c4a8552fb3cb31bb05ef40831d4c6c232adefc32
Roshith P <roshith844@gmail.com>
2022-10-21 18:46:20 +0530

    build: Login page

--- index.js
+++ index.js
@@ -2,11 +2,26 @@ const http = require('http')
 const express = require('express')
 const app = express()
 const expressLayouts = require('express-ejs-layouts')
+const session = require('express-session')
+const cookieParser = require('cookie-parser')
 
-//server
+//initialises npm modules
+app.use(cookieParser())
+app.use(session(
+     {
+          resave: true,
+          saveUninitialized: true,
+          secret: 'secret'
+     }
+))
+app.use(expressLayouts)
+app.set('layout','./layouts/page.ejs')
+app.set('view engine','ejs')
+//navigation
+app.get('/login',(req,res)=>{
+     res.render('login')
+ })
+ //server
 http.createServer(app).listen(8000,()=>{
      console.log("running")
 })
-app.use(expressLayouts)
-app.set('layout','./layout/page.ejs')
-app.set('view engine','ejs')
